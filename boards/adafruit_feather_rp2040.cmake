bfd693cbc36b8c87e7341b8316b15ebfb0fde117
omarbates7557-alt <omarbates7557@gmail.com>
2026-07-25 02:57:00 +0700

    Update adafruit_feather_rp2040.cmake

--- boards/adafruit_feather_rp2040.cmake
+++ boards/adafruit_feather_rp2040.cmake
@@ -1,7 +1,7 @@
 add_compile_definitions(
     BOARD_NAME="Adafruit Feather RP2040"
     PIO_USB_DP_PIN_DEFAULT=12
-    LED_NEOPIXEL=1
+    LED_NEOPIXEL=0
 )
 
 # MinSizeRel breaks usb_task() on RP2040...
